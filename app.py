ee0f35f3dde1041642d83438cfa3e83ce2baaacd
cauaedm <cauaedm@dcc.ufrj.br>
2024-12-09 14:23:36 -0300

    test

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Dash Twitter Scraper and Analysis - UFRJ Consulting Club")
+st.title("Dashboard Twitter Scraper and Analysis - UFRJ Consulting Club")
 
 df = pd.read_csv('tweets.csv')
 st.dataframe(df)
